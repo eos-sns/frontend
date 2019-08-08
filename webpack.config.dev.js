2d4c25f0441adfd5cfe486cb71dfba5560a16246
stefano <sirfoga@protonmail.com>
2019-08-08 09:18:21 +0200

    add webpack prod config

---
+++ webpack.config.dev.js
@@ -0,0 +1,24 @@
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
+
+module.exports = (env) => {
+  const API_HOST = "http://localhost";
+  const API_PORT = "4000";
+  const API_URL = API_HOST + ":" + API_PORT;
+
+  const mode = 'development';
+  const externals = {
+    config: JSON.stringify({
+      apiUrl: API_URL,
+      usersApiUrl: API_URL + '/users',
+      searchApiUrl: API_URL + '/search',
+      jupyterUrl: 'http://localhost:8888'
+    })
+  };
+  const newConfig = {
+    mode: mode,
+    externals: externals
+  };
+
+  return merge(baseConfig, newConfig);
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,24 @@
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
+
+module.exports = (env) => {
+  const API_HOST = "https://eos.sns.it";
+  const API_PORT = "2000";
+  const API_URL = API_HOST + ":" + API_PORT;
+
+  const mode = 'production';
+  const externals = {
+    config: JSON.stringify({
+      apiUrl: API_URL,
+      usersApiUrl: API_URL + '/users',
+      searchApiUrl: API_URL + '/search',
+      jupyterUrl: 'https://eos.sns.it/2001'
+    })
+  };
+  const newConfig = {
+    mode: mode,
+    externals: externals
+  };
+
+  return merge(baseConfig, newConfig);
+};
